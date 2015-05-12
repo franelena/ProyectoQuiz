5e63740f67b559d917511df9f640f4eb21083a8f
ElenaLoures <elenaloures@gmail.com>
2015-05-13 00:23:56 +0200

    busca preguntas (actualizado .gitignore)

--- models/models.js
+++ models/models.js
@@ -26,7 +26,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 						}
 					);
 
-//Importar la definción de la tabla Quiz en quiz.js
+// Importar la definción de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname, 'quiz')
 var Quiz = sequelize.import(quiz_path);
 
