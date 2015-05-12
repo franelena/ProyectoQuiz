33bc7e2c68a550057c415daa86e0d8d04df253f8
ElenaLoures <elenaloures@gmail.com>
2015-05-12 22:33:22 +0200

    Con búsqueda de preguntas

--- models/models.js
+++ models/models.js
@@ -39,6 +39,10 @@ sequelize.sync().then(function(){
 		if(count === 0){  //La tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+						});
+
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
 						})
 			.then(function(){console.log('Base de datos inicializada')});
 		};
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,18 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
 router.get('/author', function(req, res){
 	res.render('author', {title: 'Créditos'});
 });
 
-router.get('/quizes/question', quizController.question);
+//Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
 
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
